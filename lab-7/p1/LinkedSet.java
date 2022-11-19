8b8c8efe029a85e9dc24ff77af81ee1f00f12954
Ramona <94061613+mnoramona@users.noreply.github.com>
2022-11-19 22:07:47 +0200

    Create LinkedSet.java

---
+++ lab-7/p1/LinkedSet.java
@@ -0,0 +1,31 @@
+package lab7.problema2;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class LinkedSet extends LinkedList implements Set {
+
+
+    public boolean add(Object obj){
+        if(!this.contains(obj)){
+            return super.add(obj);
+        }
+        return false;
+    }
+
+    public void add (int i, Object obj){
+        if(!this.contains(obj)){
+            super.add(i, obj);
+        }
+    }
+
+    public Object set (int i , Object obj){
+        if(!this.contains(obj)){
+            Object objvechi = get(i);
+            super.set(i, obj);
+            return objvechi;
+        }
+        else return null;
+    }
+}
